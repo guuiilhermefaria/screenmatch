70034220bcf53815cbc691adb80a89a3c11569d5
Guilherme Faria <guuiilhermefaria@gmail.com>
2024-09-21 14:37:31 -0300

    Screen match project
    Project ongoing to store the information about movies. This
    project will be possible to rate, search, get the average about
    notes on a movie.
    
    Tracking-Id: EIP

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,53 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.Filme;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2023);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        Filme f1 = filmeDoPaulo;
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+
+        for (Titulo item : lista) {
+            System.out.println(item.getNome());
+            if(item instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificacao: " + filme.getClassificacao());
+            }
+        }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jacqueline");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois de ordenar: " + buscaPorArtista);
+
+        System.out.println("Lista de titulos ordenados pelo nome: ");
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando pelo ano de lancamento: ");
+        System.out.println(lista);
+    }
+}
